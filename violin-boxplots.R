a6fa861f8adbf5b4e2773fdf5204323171430cfa
Vasilis F. Ntasis <45040131+vntasis@users.noreply.github.com>
2022-02-10 15:54:41 +0000

    Update violin-boxplots.R
    - Add code for drawing boxplots without outliers
    - Correct identation

--- violin-boxplots.R
+++ violin-boxplots.R
@@ -14,11 +14,36 @@ ggplot(data, aes(x, y, colour = x)) +
 dt %>%
   ggplot(aes(x, y, colour = x)) +
   geom_boxplot(fill = "grey") +
-    facet_wrap(~parameter, ncol = 12) +
-    theme(axis.title.x=element_blank(),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank()) +
-    scale_color_futurama()
+  facet_wrap(~parameter, ncol = 12) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  scale_color_futurama()
+
+
+# Draw Boxplots without outliers
+# filtering function - turns outliers into NAs to be removed
+filter_lims <- function(x){
+  l <- boxplot.stats(x)$stats[1]
+  u <- boxplot.stats(x)$stats[5]
+
+  for (i in 1:length(x)){
+    x[i] <- ifelse(x[i]>l & x[i]<u, x[i], NA)
+  }
+  return(x)
+}
+
+dt %>%
+  group_by(parameter, x) %>%
+  mutate(y = filter_lims(y)) %>%
+  ggplot(aes(x, y, colour = x)) +
+  # remove NAs, and set the whisker length to all included points
+  geom_boxplot(fill = "grey", na.rm = TRUE, coef = 5) +
+  facet_wrap(~parameter, ncol = 12) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  scale_color_brewer(type = 'qual', palette='Dark2')
 
 
 # Violin - box Plot accompanied by a statistical test
