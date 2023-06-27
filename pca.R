ddd24b7696735cfe85b6b504b67f838890b09c4f
vntasis <45040131+vntasis@users.noreply.github.com>
2023-06-27 02:59:15 +0200

    Update deprecated code chunks and add code for faceted pc scatter plots

--- pca.R
+++ pca.R
@@ -1,5 +1,5 @@
 #************************************************************
-## Perform a PCA and plot the first two principal components
+## Perform a PCA and plot the principal components
 ## in a scatter plot
 #************************************************************
 
@@ -43,38 +43,67 @@ dataset_pca <- dataset %>%
 # Check the variance explained by each PC
 var_exp <- dataset_pca %>%
   unnest(pca_aug) %>%
-  summarize_at(.vars = vars(contains("edPC", ignore.case = F)), .funs = funs(var)) %>%
-  gather(key = pc, value = variance) %>%
+  summarize_at(.vars = vars(contains("edPC", ignore.case = F)),
+               .funs = ~ var(.x)) %>%
+  pivot_longer(everything(),
+               names_to = "pc", values_to = "variance") %>%
   mutate(var_exp = variance/sum(variance),
          cum_var_exp = cumsum(var_exp),
-         pc = str_replace(pc, ".fitted", ""))
+         pc = str_replace(pc, ".fitted", ""),
+         PC=factor(str_sub(pc, 3, -1), levels=1:length(pc)))
 
+# Plot variance explained
 var_exp %>%
-  mutate(PC=str_sub(pc, 3, -1) %>% as.integer) %>%
   rename(
     `Variance Explained` = var_exp,
     `Cumulative Variance Explained` = cum_var_exp
   ) %>%
-  gather(key = key, value = value, `Variance Explained`:`Cumulative Variance Explained`) %>%
-  ggplot(aes(PC, value, group = key)) +
+  pivot_longer(c(3,4)) %>%
+  ggplot(aes(PC, value, group = name)) +
   geom_point() +
   geom_line() +
   coord_flip() +
-  facet_wrap(~key, scales = "free_y") +
+  facet_wrap(~name, scales = "free_x") +
   theme_bw() +
-  labs(y = "Variance",
-       title = "Variance explained by each principal component")
+  ylab("") +
+  scale_x_discrete(labels=seq(1,nrow(var_exp),2) %>%
+                   paste0("_") %>% str_split("_") %>% unlist)
 
 
-dataset_pca %>%
-  mutate(
-    pca_graph = map2(
-      .x = pca,
-      .y = data,
-      ~ autoplot(.x,
-                 data = .y,
-                 colour="Group") +
-        labs(x = "Principal Component 1",
-             y = "Principal Component 2",
-             title = "First two principal components of PCA on x dataset")
-    )
+
+#-----------------------------------
+## Plot scatter plots for all the
+## combinations of the first 5 PCs
+## with ggplot and facets
+#-----------------------------------
+library(grid)
+library(gtable)
+
+# Reshape pca output into long tibble
+# and take the 5 first principal components
+pca_long <-
+  pca$x %>%
+  as.data.frame %>%
+  rownames_to_column("SampleID") %>%
+  pivot_longer(-SampleID) %>%
+  filter(name %in% paste0("PC", 1:5)) %>%
+  left_join(metadata)
+
+# Form tibble with all the PC combinations
+pcB <-
+  pca_long %>%
+  rename(pcB=name, PcompB=value) %>%
+  select(-Group)
+
+pca_long %<>%
+  rename(pcA=name, PcompA=value) %>%
+  left_join(pcB)
+
+rm(pcB)
+
+# Make faceted scatter plots
+pca_long %>%
+  ggplot(aes(PcompA, PcompB, col = Group, fill = Group)) +
+  stat_ellipse(geom = "polygon", alpha = 0.5) +
+  geom_point(shape = 21, col = "black") +
+  facet_grid(cols = vars(pcA), rows = vars(pcB))
